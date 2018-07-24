0b742526018b688c2e046c5ace12bd54c08f0650
Amrutha hebbar <hebbaramrutha@gmail.com>
2018-07-24 13:03:24 +0530

    Initial commit

---
+++ SaveAndOpenRibbon.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new SaveAndOpenRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace WordAddIn
+{
+    [ComVisible(true)]
+    public class SaveAndOpenRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public SaveAndOpenRibbon()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("WordAddIn.SaveAndOpenRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        public void OnSave(Office.IRibbonControl control)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream stream = fileDialog.OpenFile())
+                {
+                    StreamWriter file = new StreamWriter(fileDialog.FileName.ToString());
+                    file.Close();
+                }
+            }
+        }
+
+        public void OnOpen(Office.IRibbonControl control)
+        {
+            var application = Globals.ThisAddIn.Application;
+            var fileDialog = new OpenFileDialog();
+            var file = fileDialog.ShowDialog();
+
+            if (file == DialogResult.OK)
+            {
+                application.Documents.Open(fileDialog.FileName);
+            }
+        }
+    }
+}
